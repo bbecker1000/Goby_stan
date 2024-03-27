c2ec37f221c5b98aee5a9b301ead17636951cf3e
Becker <BenBecker@nps.gov>
2024-03-27 14:15:40 -0700

    diagnostic plots

--- Code/3b_plot_chains.R
+++ Code/3b_plot_chains.R
@@ -7,30 +7,125 @@ chain.data <- chain.data[c(3001:6000),]
 #add rownames
 chain.data$sample <- 1:3000
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_BreachDays))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_BreachDays)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_Wind))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_Wind)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_SAV))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_SAV)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_DO))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_DO)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_Micro))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_Micro)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_Substrate))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_Substrate)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
 
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_Goby_lag))
 chain.data %>%
   ggplot(aes(x=sample, y=beta_Goby_lag)) +
-  geom_line(alpha = 0.2)
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
+
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_SB_count))
+chain.data %>%
+  ggplot(aes(x=sample, y=beta_SB_count)) +
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
+
+MEAN <- chain.data %>% summarize(MEAN = mean(beta_SC_count))
+chain.data %>%
+  ggplot(aes(x=sample, y=beta_SC_count)) +
+  geom_line(alpha = 0.2) +
+  geom_hline(yintercept = as.numeric(MEAN), linetype = 2)
+
+
+
+
+#Pairs plots
+cowplot::plot_grid(
+  
+  chain.data %>%
+  ggplot(aes(x=beta_BreachDays, y=beta_SC_count)) +
+    geom_point(color = "blue",alpha = 0.2)
+,
+
+chain.data %>%
+  ggplot(aes(x=beta_SB_count, y=beta_Goby_lag)) +
+    geom_point(color = "blue",alpha = 0.2)
+,
+
+chain.data %>%
+  ggplot(aes(x=beta_BreachDays, y=beta_SC_count)) +
+    geom_point(color = "blue",alpha = 0.2)
+,
+
+chain.data %>%
+  ggplot(aes(x=phi, y=tau)) +
+    geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_Micro, y=beta_BreachDays)) +
+    geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_Wind, y=beta_BreachDays)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_Year, y=beta_BreachDays)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_Year, y=beta_Rain)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=mu_Zone, y=tau_Zone)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_SAV, y=beta_Substrate)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_DO, y=beta_Temp)) +
+  geom_point(color = "blue",alpha = 0.2)
+,
+chain.data %>%
+  ggplot(aes(x=beta_Goby_lag, y=beta_Year)) +
+  geom_point(color = "blue",alpha = 0.2)
+
+)
+
+
+
+
+
+
+
+
+
+
